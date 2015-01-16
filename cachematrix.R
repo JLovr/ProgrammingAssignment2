ab6f87198dfe2d604b935524bc3d3e506975f9eb
John Lovrinic <jjlj422@gmail.com>
2015-01-16 00:07:57 -0600

    Week 2 Solution code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## creates a cached list that contains a matrix and its inverse
+## which are employed by cachSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+          x <<- y
+          m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## finds the inverse of an invertible matrix, using cached
+## values if available, else calculating and caching the new value
+## in a cached list managed by makeCacheMatrix.
+## Generates an error if matrix is not invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Return a matrix m that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
